171e38e52f03ad24f7c7d4eca92463259b73d7ef
banimation <sw006612@gmail.com>
2023-11-12 20:42:03 +0900

    11/12

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,33 +1,38 @@
-import express from 'express'
+import express, { json } from 'express'
 import path from 'node:path'
 import * as fs from 'node:fs'
 import multer from 'multer'
 
 const server = express()
-const upload = multer({
+const Upload = multer({
     storage: multer.diskStorage({
       	filename(_req, _file, done) {
-            const filePath = path.join(__dirname, "../public")
+            const filePath = path.join(__dirname, "../../front/public/picture")
             const number = (fs.readdirSync(filePath)).length
           	// console.log(file)
 			done(null, `${number}.png`)
         },
 		destination(_req, _file, done) {
-		    done(null, path.join(__dirname, "../public"))
+		    done(null, path.join(__dirname, "../../front/public/picture"))
 	    },
     }),
     // limits: { fileSize: 1024 * 1024 },
-});
-const uploadMiddleware = upload.single("picture")
+}).single("picture")
 
-server.use(express.urlencoded({ extended: false }), express.json(), uploadMiddleware)
+server.use(express.urlencoded({ extended: false, limit:"5mb" }), express.json({ limit:"5mb" }), Upload)
 
 server.get("/", (_req, res) => {
     res.sendFile(path.join(__dirname, "../../front/public/html/index.html"))
 })
 
-server.post("/sendPicture", uploadMiddleware, (req, _res) => {
-    console.log(req.file)
+server.post("/sendPicture", Upload, (_req, res) => {
+    res.send("done")
+})
+
+server.post("/createResult", (req, res) => {
+    const b64:string = (req.body.b64).split(",")[1]
+    fs.writeFileSync("../result/result.png", b64, {encoding: "base64"})
+    res.send("done")
 })
 
 server.use(express.static(`${__dirname}/../../front/public`))
--- front/src/index.ts
+++ front/src/index.ts
@@ -1,26 +1,28 @@
-const main = document.getElementById("main") as HTMLElement
+import mergeImages from "merge-images"
+import * as fs from "node:fs"
 
+const main = document.getElementById("main") as HTMLElement
 const cameraContainer = document.getElementById("camera-container") as HTMLElement
 const cam = document.getElementById("cam") as HTMLVideoElement
-
 const startBtn = document.getElementById("start-btn") as HTMLElement
 const cursor = document.getElementById("cursor") as HTMLElement
-
 const canvas = document.getElementById("canvas") as HTMLCanvasElement
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D
-
 const time = document.getElementById("time") as HTMLElement
 const count = document.getElementById("count") as HTMLElement
+const blackScreen = document.getElementById("black") as HTMLElement
+const images = document.getElementById("images") as HTMLElement
+const complete = document.getElementById("complete") as HTMLElement
 
-const maxCount = 6
+const maxCount = 4
 const maxTime = 1
 let currentCount = 0
 
-
 const width = 1000
 let height: number
-
 let isStreaming = false
+
+blackScreen.classList.add("hidden")
 
 if(navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({ video: true })
@@ -48,7 +50,6 @@ const takePicture = () => {
         if(blob) {
             const formData = new FormData()
             formData.append("picture", blob)
-            console.log(blob)
             fetch(`/sendPicture`, {
                 method: "POST",
                 headers: {
@@ -65,10 +66,17 @@ const perform = async () => {
     for(let i = 0; i < maxCount; i++) {
         for(let n = 0; n < maxTime + 1; n++) {
             await new Promise((res, _rej) => {
-                setTimeout(() => {
+                setTimeout(async () => {
                     if(timeRemaining <= 0) {
                         time.innerText = ""
-                        takePicture()
+                        blackScreen.classList.remove("hidden")
+                        await new Promise((res, _rej) => {
+                            setTimeout(() => {
+                                takePicture()
+                                blackScreen.classList.add("hidden")
+                                res("")
+                            }, 2000)
+                        })
                         currentCount += 1
                         count.innerText = `${currentCount}/${maxCount}`
                         timeRemaining = maxTime
@@ -81,14 +89,62 @@ const perform = async () => {
             })
         }
     }
-    
 }
-
+const selectedImg: Array<number> = []
 startBtn.addEventListener('click', () => {
     main.classList.add("hidden")
     cameraContainer.classList.remove("hidden")
-    perform()
+    perform().then(() => {
+        time.innerText = "✔"
+        setTimeout(() => {
+            cameraContainer.classList.add("hidden")
+            for(let i = 0; i < maxCount; i++) {
+                const img = document.createElement("img") as HTMLImageElement
+                
+                img.style.width = "200px"
+                img.src = `/picture/${i}.png`
+                img.classList.add("img")
+                images.append(img)
+
+                img.addEventListener("click", () => {
+                    if(!(selectedImg.includes(i))) {
+                        if(selectedImg.length < 4) {
+                            selectedImg.push(i)
+                            img.classList.add("selected")
+                        }
+                    } else {
+                        const index = selectedImg.indexOf(i)
+                        if (index > -1) {
+                            selectedImg.splice(index, 1)
+                            img.classList.remove("selected")
+                        }
+                    }
+                })
+            }
+        }, 3000)
+    })
 })
+complete.addEventListener("click", () => {
+    if(selectedImg.length === 4) {
+        mergeImages([
+            {src: "/frame/frame.png", x: 0, y: 0},
+            {src: `/picture/${selectedImg[0]}.png`, x: 10, y: 50},
+            {src: `/picture/${selectedImg[1]}.png`, x: 10, y: 100},
+            {src: `/picture/${selectedImg[2]}.png`, x: 10, y: 150},
+            {src: `/picture/${selectedImg[3]}.png`, x: 10, y: 200},
+        ]).then((b64) => {
+            console.log(b64)
+            fetch("createResult", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({b64})
+            })
+        })
+    }
+})
+
 window.addEventListener("mousemove", (event) => {
     cursor.style.left = `${event.clientX}px`
     cursor.style.top = `${event.clientY}px`
