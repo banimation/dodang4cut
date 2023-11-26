a6354ad1b4eefb68d86b71919d2564e708c9c068
banimation <sw006612@gmail.com>
2023-11-27 00:26:22 +0900

    1차 완성본

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,7 +1,8 @@
-import express, { json } from 'express'
+import express from 'express'
 import path from 'node:path'
 import * as fs from 'node:fs'
 import multer from 'multer'
+import sharp from 'sharp'
 
 const server = express()
 const Upload = multer({
@@ -14,6 +15,7 @@ const Upload = multer({
         },
 		destination(_req, _file, done) {
 		    done(null, path.join(__dirname, "../../front/public/picture"))
+            done(null, path.join(__dirname, "../public/picture"))
 	    },
     }),
     // limits: { fileSize: 1024 * 1024 },
@@ -25,14 +27,49 @@ server.get("/", (_req, res) => {
     res.sendFile(path.join(__dirname, "../../front/public/html/index.html"))
 })
 
+server.get("/result", (_req, res) => {
+    res.sendFile(path.join(__dirname, "../../front/public/html/result.html"))
+})
+
 server.post("/sendPicture", Upload, (_req, res) => {
     res.send("done")
 })
 
-server.post("/createResult", (req, res) => {
-    const b64:string = (req.body.b64).split(",")[1]
-    fs.writeFileSync("../result/result.png", b64, {encoding: "base64"})
-    res.send("done")
+server.post("getResult", (_req, res) => {
+    res.sendFile(path.join(__dirname, "../public/result/result.png"))
+})
+let isconnect = false
+server.post("/createResult", async (req, res) => {
+    if(!isconnect) {
+        isconnect = true
+        const selectedImg = req.body.selectedImg
+        const width = 2000
+        const height = 1500
+        selectedImg.map(async (i: number) => {
+            try {
+                await sharp(`../public/picture/${selectedImg[i]}.png`)
+                    .resize({width: width, height: height})
+                    .toFile(`../public/composite/${selectedImg[i]}.png`)
+            } catch (err) {
+                console.log(err)
+            }
+            
+        })
+        const layers = selectedImg.map((i: number) => ({input: `../public/composite/${selectedImg[i]}.png`, left: 134, top: height * (i) + 80 * (i + 1)}))
+        await sharp("../public/frame/frame.png")
+            .composite(layers)
+            .toFile("../public/result/result.png")
+        for(let i = 0; i < 4; i++) {
+            try {
+                fs.unlinkSync(`../public/picture/${i}.png`)
+                fs.unlinkSync(`../../front/public/picture/${i}.png`)
+            } catch(err) {
+                console.log(err)
+            }
+        }
+        isconnect = false
+        res.send("done")
+    }
 })
 
 server.use(express.static(`${__dirname}/../../front/public`))
